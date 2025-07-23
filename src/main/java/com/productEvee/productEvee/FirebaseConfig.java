701d0ea017402e00d046c16b7ca64a2756504ef0
leandro soares <leandro20002004@hotmail.com>
2025-07-23 17:07:37 -0300

    feat:

--- src/main/java/com/productEvee/productEvee/FirebaseConfig.java
+++ src/main/java/com/productEvee/productEvee/FirebaseConfig.java
@@ -1,14 +1,9 @@
 package com.productEvee.productEvee;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.FirestoreClient;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -17,40 +12,23 @@ import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
+@PostConstruct
+public void init() throws IOException {
+    InputStream serviceAccount = getClass().getClassLoader()
+        .getResourceAsStream("producteve-65ffb-firebase-adminsdk-fbsvc-2469a53581.json");
 
-    @PostConstruct
-    public void init() {
-        try {
-            InputStream serviceAccount = getClass().getClassLoader()
-    .getResourceAsStream("producteve-65ffb-firebase-adminsdk-fbsvc-2469a53581.json");
+    if (serviceAccount == null) {
+        throw new IllegalStateException("Arquivo de credenciais Firebase não encontrado no classpath!");
+    }
 
-if (serviceAccount == null) {
-    throw new IllegalStateException("Arquivo de credenciais Firebase não encontrado no classpath!");
+    FirebaseOptions options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        .build();
+
+    if (FirebaseApp.getApps().isEmpty()) {
+        FirebaseApp.initializeApp(options);
+        System.out.println("Firebase inicializado com sucesso!");
+    }
 }
 
-        FirebaseOptions options = FirebaseOptions.builder()
-    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-    .build();
-
-
-
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-                System.out.println("Firebase inicializado com sucesso!");
-            }
-
-        } catch (IOException e) {
-            System.err.println("Erro ao inicializar o Firebase: " + e.getMessage());
-        }
-    }
-
-    @Bean
-    public Firestore getFirestore() {
-        return FirestoreClient.getFirestore();
-    }
-
-    @Bean
-    public DatabaseReference getDatabaseReference() {
-        return FirebaseDatabase.getInstance().getReference();
-    }
 }
